d5f2b6684b351bee7e50cc7678d21cb3f322cfde
skylers27 <32472386+skylers27@users.noreply.github.com>
2018-10-08 15:37:43 -0400

    Arduino code

---
+++ wall_follow1.ino
@@ -0,0 +1,83 @@
+int directionL = 4;
+int speedL = 5;
+int directionR = 7;
+int speedR = 6;
+int photoL = A6;
+int photoR = A0;
+int photoB = A7;
+int sharpL = A3;
+int sharpM = A1;
+int sharpR = A2;
+int ir = A4;
+int sound = A5;
+int flameLED = 3;
+int soundLED = 4;
+int sd = 150;
+int xf = 500;
+int xr = 500;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  digitalWrite(directionL, LOW);
+  digitalWrite(directionR, LOW);
+  pinMode(flameLED, OUTPUT);
+  pinMode(soundLED, OUTPUT);
+}
+
+void loop() {
+int soundval = analogRead(sound);
+  if (soundval<10){
+    digitalWrite(soundLED, HIGH);   // turn the LED on (HIGH is the voltage level)
+    delay(1000);                       // wait for a second
+    digitalWrite(soundLED, LOW);
+  }
+int flameval = analogRead(ir); 
+  if (flameval<100){
+    digitalWrite(flameLED, HIGH);   // turn the LED on (HIGH is the voltage level)
+    delay(1000);                       // wait for a second
+    digitalWrite(flameLED, LOW);
+  }
+  
+  //analogWrite(speedL,sd);
+  //analogWrite(speedR,sd);
+  int distanceM;
+  int distanceL;
+  int distanceR;
+   //put your main code here, to run repeatedly:
+  distanceM = analogRead(sharpM);
+  distanceL = analogRead(sharpL);
+  distanceR = analogRead(sharpR);
+
+Serial.println(distanceM);
+
+  
+  if (distanceM>450)
+  {
+
+    rotateLeft();
+    
+  }
+  else
+  {
+        
+    if (distanceR>300)
+    {
+      slightLeft(sd);
+    }
+    else if (distanceR<300 && distanceR>100)
+    {
+       forward();
+    }
+    else if (distanceR<100)
+    {
+      slightRight(sd);
+    }
+    }
+    
+    
+    delay(100);
+  
+} 
+ 
+ 
---
+++ wall_follow_helpers.ino
@@ -0,0 +1,59 @@
+void setspeed(int s)
+{
+  analogWrite(speedL, s);
+  analogWrite(speedR, s);
+}
+void brakeON()
+{
+  digitalWrite(directionL, LOW);
+  digitalWrite(directionR, LOW);
+  setspeed(0);
+
+}
+void brakeOFF()
+{
+  digitalWrite(directionL, LOW);
+  digitalWrite(directionR, LOW);
+  setspeed(100);
+}
+void forward()
+{
+  brakeOFF();
+  
+  setspeed(100);
+}
+void backward()
+{
+  brakeOFF();
+  digitalWrite(directionL, HIGH);
+  digitalWrite(directionR, HIGH);
+  setspeed(100);
+}
+void rotateLeft()
+{
+  digitalWrite(directionL, HIGH);
+  digitalWrite(directionR, LOW);
+  setspeed(100);
+  
+}
+void rotateRight()
+{
+  digitalWrite(directionL, LOW);
+  digitalWrite(directionR, HIGH);
+  setspeed(100);
+  delay(200);
+}
+void slightLeft(int velocity)
+{
+  brakeON();
+  analogWrite(speedL, .50 * velocity);
+  analogWrite(speedR, velocity);
+  delay(100);
+}
+void slightRight(int velocity)
+{
+  brakeON();
+  analogWrite(speedL, velocity);
+  analogWrite(speedR ,.50 * velocity);
+  delay(100);
+}
